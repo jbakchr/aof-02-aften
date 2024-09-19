236efdb13087ebc91336fa6bc9e30c5d44d23f59
Jonas Bak Phillipson <jbakchr@gmail.com>
2024-09-19 22:29:02 +0200

    Added comment examples

---
+++ 01-examples/03-comments/main.py
@@ -0,0 +1,9 @@
+# Denne linje er et eksempel på en kommentar .. (hvis formål gerne skal være at forklare sin kode under kommentaren)
+comment = "Wauw!"
+
+
+inline_comment = "Endnu mere .. Wauw!"  # Eksempel på kommentar der forklarer kodelinje
+
+
+# Nedenstående er et eksempel på kode som er "udkommenteret" vhja. en kommentar
+# is_executed = "Nope .."
